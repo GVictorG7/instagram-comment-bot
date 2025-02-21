24b2be6941da5d2de7a39c13972207c2f1740065
Victor Georgescu <victor.georgescu07@gmail.com>
2025-02-21 12:19:18 +0200

    Fix Followers and Following buttons locators

--- src/test/java/com/victor/instagramcommentbot/page/InstagramMyProfilePage.java
+++ src/test/java/com/victor/instagramcommentbot/page/InstagramMyProfilePage.java
@@ -6,12 +6,12 @@ import com.codeborne.selenide.SelenideElement;
 import static com.codeborne.selenide.Selenide.$x;
 
 public class InstagramMyProfilePage {
-  public SelenideElement followersSectionButton = $x("//a[contains(text(), ' followers')]");
+  public SelenideElement followersSectionButton = $x("//span[contains(text(), ' followers')]");
   public SelenideElement followersSectionPanel =
       $x("//div[contains(text(), 'Followers')]/../../../../../div[3]");
   public ElementsCollection followerElementList = followersSectionPanel.$$("div");
 
-  public SelenideElement followingSectionButton = $x("//a[contains(text(), ' following')]");
+  public SelenideElement followingSectionButton = $x("//span[contains(text(), ' following')]");
   public SelenideElement followingSectionPanel =
       $x("//div[contains(text(), 'Following')]/../../../../../div[3]");
   public ElementsCollection followingElementList = followingSectionPanel.$$("div");
